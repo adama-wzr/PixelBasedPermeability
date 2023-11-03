b0201a0fb9de6a759121bd5b8a6ca3f90d47abc1
adama-wzr <andrefadam@gmail.com>
2023-11-03 00:53:26 -0500

    -Updated
    What did I change?

--- Convergence.py
+++ Convergence.py
@@ -10,17 +10,23 @@ df1 = pd.read_csv("ConvergenceData.csv")
 
 raw_data = df1[["iter", "K", "R", "alpha", "mesh"]].to_numpy()
 
+# Get final permeability
+
+perm = f"Perm={raw_data[-1,1]:07f}"
+
 fig1, (ax1, ax2) = plt.subplots(1, 2, tight_layout=True)
 
 fig1.set_dpi(100)
 fig1.set_size_inches(6, 3)
 
 ax1.plot(raw_data[:, 0], raw_data[:,1], 'b*')
-ax1.set_xlabel('Iterations', fontsize=14)
-ax1.set_ylabel(r'k$^{*}[/]$', fontsize=14)
+ax1.set_title('File 00001.jpg', fontsize=16)
+ax1.set_xlabel('Iterations', fontsize=16)
+ax1.set_ylabel(r'k$^{*}[/]$', fontsize=16)
 
 ax2.plot(raw_data[:, 0], raw_data[:,2], 'k--')
-ax2.set_xlabel('Iterations', fontsize=14)
-ax2.set_ylabel('Residual', fontsize=14)
+ax2.set_xlabel('Iterations', fontsize=16)
+ax2.set_ylabel('Residual', fontsize=16)
+ax2.set_title(perm, fontsize=16)
 
 plt.show()
